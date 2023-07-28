361065563bc753e21556bb49ce567d80bc7bde50
Lee <leejavaa@gmail.com>
2023-07-28 03:42:32 +0200

    Added projects and project data

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,18 +1,22 @@
+import { ProjectCard } from '@/components/ProjectCard'
+
 export default function Page() {
     return (
         <main className="w-screen">
                 <h1 className="ml-60 mt-24 text-6xl font-bold">Projects</h1>
-                <section id="project-grid" className="w-3/4 mx-auto mt-12">
-                    <div className="bg-white rounded-lg w-1/4 h-fit text-black p-4">
-                        <div className="w-full h-52 bg-black rounded-lg mb-4" />
-                        <div className="flex flex-col gap-4">
-                        <h1 className="text-2xl font-bold">RC Car</h1>
-                        <p>A remote control car developed for university</p>
-                        <button className="rounded-lg bg-black text-white px-11 py-3 font-bold text-lg">
-                            vroom
-                        </button>
-                        </div>
-                    </div>
+                <section id="project-grid" className="w-3/4 mx-auto mt-12 flex gap-10">
+                    <ProjectCard id={0} />
+                    <ProjectCard id={1} />
+                    <ProjectCard id={2} />
+                </section>
+                <section id="project-grid" className="w-3/4 mx-auto mt-12 flex gap-10">
+                    <ProjectCard id={3} />
+                    <ProjectCard id={4} />
+                    <ProjectCard id={5} />
+                </section>
+                <section id="project-grid" className="w-3/4 mx-auto mt-12 flex gap-10">
+                    <ProjectCard id={6} />
+                    <ProjectCard id={7} />
                 </section>
         </main>
     )
---
+++ src/app/projects/project/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <main className="w-screen">
+            <h1>Project Title</h1>
+            <h2>Project Description</h2>
+            <h3>Skills Involved</h3>
+            <h4>?paper</h4>
+        </main>
+    )
+}
