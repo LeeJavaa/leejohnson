1aef9d1258647b44126d01e3537ef4933a73b0cd
Lee <leejavaa@gmail.com>
2023-09-15 09:51:55 +0200

    Finished projects page, without links

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -5,42 +5,42 @@ export default function Home() {
   return (
     <main className="w-full h-screen">
       <div className="absolute left-0 right-0 bottom-0 top-0 grid grid-cols-2 lg:grid-cols-8 2xl:grid-cols-10 grid-rows-5 text-sm">
-        <h1 className="col-start-1 lg:col-start-3 2xl:col-start-4 row-span-1 pb-3 self-end mx-4 z-50 text-base">
-          PROJECTS
+        <h1 className="col-start-1 lg:col-start-3 2xl:col-start-4 row-span-1 pb-3 self-end mx-3 z-50 text-base">
+          NOTABLE PROJECTS
         </h1>
         <Link
           href={"/projects/remote-controlled-car"}
-          className="col-start-1 row-start-2 col-span-1 row-span-1 lg:col-start-3 2xl:col-start-4 px-4 pt-2 hover:underline hover:cursor-pointer z-50"
+          className="col-start-1 row-start-2 col-span-1 row-span-1 lg:col-start-3 2xl:col-start-4 px-4 pt-2 hover:underline hover:cursor-pointer z-50 bg-[url('../images/rc-thumbnail.png')] bg-contain bg-center grayscale hover:grayscale-0 duration-500"
         >
           remote controlled car
         </Link>
         <Link
-          href={"/projects/whatsbuzzing"}
-          className="col-start-2 row-start-2 col-span-1 row-span-1 lg:col-start-5 2xl:col-start-6 px-4 pt-2 hover:underline hover:cursor-pointer z-50"
-        >
-          whats buzzing?
-        </Link>
-        <Link
-          href={"/projects/untitled"}
-          className="col-start-1 row-start-3 col-span-1 row-span-1 lg:col-start-4 2xl:col-start-5 px-4 pt-2 hover:underline hover:cursor-pointer z-50"
-        >
-          untitled
-        </Link>
-        <Link
           href={"/projects/multimeter-and-signal-generator"}
-          className="col-start-2 row-start-3 col-span-1 row-span-1 lg:col-start-6 2xl:col-start-7 px-4 pt-2 hover:underline hover:cursor-pointer z-50"
+          className="col-start-2 row-start-2 col-span-1 row-span-1 lg:col-start-5 2xl:col-start-6 px-4 pt-2 hover:underline hover:cursor-pointer z-50 bg-[url('../images/mm-thumbnail.png')] bg-contain bg-center grayscale hover:grayscale-0 duration-500"
         >
           multimeter and signal generator
         </Link>
         <Link
+          href={"/projects/untitled"}
+          className="col-start-1 row-start-3 col-span-1 row-span-1 lg:col-start-4 2xl:col-start-5 px-4 pt-2 hover:underline hover:cursor-pointer z-50 bg-[url('../images/untitled-thumbnail.png')] bg-contain bg-center grayscale hover:grayscale-0 duration-500"
+        >
+          untitled
+        </Link>
+        <Link
+          href={"/projects/whatsbuzzing"}
+          className=" col-start-2 row-start-3 col-span-1 row-span-1 lg:col-start-6 2xl:col-start-7 px-4 pt-2 px-4 pt-2 hover:underline hover:cursor-pointer z-50 bg-[url('../images/whatsbuzzing-thumbnail.png')] bg-contain bg-center grayscale hover:grayscale-0 duration-500"
+        >
+          <p>whats buzzing?</p>
+        </Link>
+        <Link
           href={"/projects/machine-learning-lecture-review"}
-          className="col-start-1 row-start-4 col-span-1 row-span-1 lg:col-start-3 2xl:col-start-4 px-4 pt-2 hover:underline hover:cursor-pointer z-50"
+          className="col-start-1 row-start-4 col-span-1 row-span-1 lg:col-start-3 2xl:col-start-4 px-4 pt-2 hover:underline hover:cursor-pointer z-50 bg-[url('../images/ml-thumbnail.png')] bg-contain bg-center grayscale hover:grayscale-0 duration-50"
         >
           machine learning lecture review
         </Link>
         <Link
           href={"/projects/portfolio-website"}
-          className="col-start-2 row-start-4 col-span-1 row-span-1 lg:col-start-5 2xl:col-start-6 px-4 pt-2 hover:underline hover:cursor-pointer z-50"
+          className="col-start-2 row-start-4 col-span-1 row-span-1 lg:col-start-5 2xl:col-start-6 px-4 pt-2 hover:underline hover:cursor-pointer z-50 bg-[url('../images/portfolio-thumbnail.png')] bg-contain bg-center grayscale hover:grayscale-0 duration-500"
         >
           portfolio website
         </Link>
