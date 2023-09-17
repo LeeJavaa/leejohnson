c07c7fdf433d167eb37f3ecc03cff9726a98bba7
Lee <leejavaa@gmail.com>
2023-09-17 08:53:52 +0200

    Updated contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
         <h1 className=" col-start-1 lg:col-start-3 2xl:col-start-4 row-span-1 self-end mx-4 z-50">
           CONTACT ME
         </h1>
-        <form className=" row-start-2 row-span-4 lg:row-span-3 col-start-1 col-end-3 lg:col-start-3 2xl:col-start-4 lg:col-end-7 2xl:col-end-8 self-start lg:self-center flex flex-col gap-5 mx-4 z-50 pt-10 lg:pt-0 text-sm lg:text-base">
+        {/* <form className=" row-start-2 row-span-4 lg:row-span-3 col-start-1 col-end-3 lg:col-start-3 2xl:col-start-4 lg:col-end-7 2xl:col-end-8 self-start lg:self-center flex flex-col gap-5 mx-4 z-50 pt-10 lg:pt-0 text-sm lg:text-base">
           <input
             type="text"
             className="bg-light font-raleway pl-2 py-2 outline-none text-veryDark rounded-[4px]"
@@ -33,7 +33,27 @@ export default function Home() {
           >
             SEND
           </button>
-        </form>
+        </form> */}
+        <div className="row-start-2 row-span-4 lg:row-span-3 col-start-1 col-end-3 lg:col-start-3 2xl:col-start-4 lg:col-end-7 2xl:col-end-8 self-start flex flex-col gap-5 mx-4 z-50 pt-10 lg:pt-0 lg:mt-5 text-sm lg:text-base text-justify">
+          <p>
+            If you'd like to contact me, please do so on LinkedIn or via email.
+            While seemingly most will have a different view, I love chatting to
+            those who want to connect, for any reason whatsoever. So please do
+            not hesitate to send me an email or pop me a DM at any time.{" "}
+          </p>
+          <div>
+            <div className="flex justify-between">
+              <p>LinkedIn:</p>
+              <a href="https://www.linkedin.com/in/lee-johnson-engineering/">
+                lee-johnson-engineering
+              </a>
+            </div>
+            <div className="flex justify-between">
+              <p>Email:</p>
+              <p className="">leejohnson19@icloud.com</p>
+            </div>
+          </div>
+        </div>
       </div>
       <Grid items={["header", "close"]} />
     </main>
