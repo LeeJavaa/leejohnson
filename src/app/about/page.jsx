cb69c140585fa780203045aafe7a12adc0c4da77
Lee <leejavaa@gmail.com>
2023-09-18 07:40:07 +0200

    Updated about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,29 +8,30 @@ export default function Home() {
           ABOUT ME
         </h1>
         <div
-          className=" row-start-2 row-span-4 lg:row-span-3 col-start-1 col-end-3 lg:col-start-3 2xl:col-start-4 lg:col-end-7 2xl:col-end-8 self-start lg:self-center pt-10 lg:pt-0 flex flex-col gap-5 lg:gap-6 2xl:gap-11 mx-4 z-50 text-sm lg:text-base animate-fadeInLong opacity-0"
+          className=" row-start-2 row-span-4 lg:row-span-3 col-start-1 col-end-3 lg:col-start-3 2xl:col-start-4 lg:col-end-7 2xl:col-end-8 self-start lg:self-center pt-4 lg:pt-0 flex flex-col gap-5 lg:gap-6 2xl:gap-11 mx-4 z-50 text-sm lg:text-base animate-fadeInLong opacity-0"
           style={{ animationFillMode: "forwards", animationDelay: "0.5s" }}
         >
           <p>
-            I am an electrical engineer by training and a software engineer by
-            passion.
+            My name is Lee Johnson, I am an electrical and software engineer by
+            training, but mostly by passion. I enjoy solving difficult and
+            complex problems that span across disciplines. My current interests
+            lie in the application of Artificial Intelligence and Machine
+            learning, blockchain and smart contract technology and the
+            intersection between hardware and software in embedded products.
           </p>
           <p>
-            I enjoy solving complex problems that are multidisciplinary in
-            nature and make life easier for every day people. I am currently
-            interested in Artificial Intelligence and Machine Learning,
-            Applications of Blockchain and Software in Gaming.
+            At the University of Stellenbosch, where I am completing my BEngHons
+            degree in Electrical Engineering, I&apos;ve garnered experience in:
+            Hand and simulated circuit design, embedded software engineering and
+            the development of software on all levels from Assembly to Python.
+            My final year project is focused on using Machine Learning and AI
+            methods to help lecturers improve their lecture videos.
           </p>
           <p>
-            At the University of Stellenbosch I’ve worked on a plethora of
-            projects ranging from vanilla electronics to embedded engineering.
-            My final year project is focused on using Machine Learning to
-            improve video lectures for student consumption.
-          </p>
-          <p>
-            My passion for Software has lead me to build many projects alone,
-            even extending to launching an app that ran in beta for two weeks.
-            It’s pushed me to spend summers interning at startups.
+            My passion for engineering has lead me to build many projects out of
+            interest, even launching some of them as products. It&apos;s pushed
+            me to spend summers interning at startups, where I&apos;ve gained
+            invaluable knowledge about engineering in industry.
           </p>
           <a href="https://github.com/LeeJavaa" target="_blank">
             <div
